0043f47b44caa2fb44ae393f542b55d4ad23cd65
dickm19 <maryclairedick@gmail.com>
2020-11-17 14:01:11 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import objects from './objects'
+
 import './App.css';
-function App() {
-  return <h1>You Can Do This!</h1>;
+
+class App extends React.Component{
+
+  state = {
+    display: 'no',
+    image: objects.no["no-image"],
+    statement: objects.no["no-statement"]
+  }
+
+  renderOpp = () => {
+    if (this.state.display == "no"){
+      this.setState({
+        display: 'yes',
+        image: objects.yes["yes-image"],
+        statement: objects.yes["yes-statement"]
+      })
+    }else{
+      this.setState({
+        display: 'no',
+        image: objects.no["no-image"],
+        statement: objects.no["no-statement"]
+      })
+    }
+
+    
+  }
+
+  render(){
+    
+    return (
+      <div>
+        <h1>{this.state.statement}</h1>
+        <img src={`${this.state.image}`} onClick={this.renderOpp}></img>
+      </div>
+        
+     
+    )
+  }
+
 }
 
 export default App;
